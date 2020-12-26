bb3ffa919cd775cd89cd9275390dcbfce327c7dc
Mikin Patel <mikinr1991@outlook.com>
2020-12-26 17:16:50 +1030

    Generics

---
+++ MyShop.Core/Contacts/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyShop.Core.Models;
+using System.Linq;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IQueryable<T> Collection();
+        void Commit();
+        void Delete(string Id);
+        void Insert(T t);
+        void Update(T t);
+    }
+}
